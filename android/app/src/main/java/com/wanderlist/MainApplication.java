fb0b43a67473720a853fdd831b67f3c9e758b065
Steven Yulong Yan <yulong.yan@uqconnect.edu.au>
2020-08-31 11:12:57 +1000

    Login screens now work on iOS

--- android/app/src/main/java/com/wanderlist/MainApplication.java
+++ android/app/src/main/java/com/wanderlist/MainApplication.java
@@ -4,10 +4,13 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+
+// What are these?
+
+// import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+// import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+// import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+// import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
