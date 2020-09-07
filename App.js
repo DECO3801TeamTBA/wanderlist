d926b1f58e12e5cd10416d23e0c4442e4480994d
Steven Yulong Yan <yulong.yan@uqconnect.edu.au>
2020-09-07 20:09:39 +1000

    Screens for stack navigation

--- App.js
+++ App.js
@@ -73,23 +73,52 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import RootStackScreen from './app/screens/RootStackScreen';
+// import RootStackScreen from './app/screens/RootStackScreen';
 // import HomeScreen from './app/screens/HomeScreen';
-import ProfileScreen from './app/screens/ProfileScreen';
-import WanderListScreen from './app/screens/WanderListScreen';
-import AddScreen from './app/screens/AddScreen';
-import RewardScreen from './app/screens/RewardScreen';
+// import ProfileScreen from './app/screens/ProfileScreen';
+// import WanderListScreen from './app/screens/WanderListScreen';
+// import AddScreen from './app/screens/AddScreen';
+// import RewardScreen from './app/screens/RewardScreen';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/Ionicons';
+// import {Button} from 'react-native-paper';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button
+        title="WanderList Screen"
+        onPress={() => navigation.navigate('WanderList')}
+      />
+    </View>
+  );
+};
+
+const WanderListScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>WanderList Screen</Text>
+    </View>
+  );
+};
+
+const RewardScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Reward Screen</Text>
+    </View>
+  );
+};
+
+const ProfileScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Profile Screen</Text>
     </View>
   );
 };
@@ -100,58 +129,64 @@ const Tab = createBottomTabNavigator();
 function App() {
   return (
     <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="WanderList" component={WanderListScreen} />
+        <Stack.Screen name="Reward" component={RewardScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
       {/* <RootStackScreen /> */}
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarIcon: ({color}) => (
-              <Icon name="earth" color={color} size={28} />
-            ),
-          }}
-        />
+      {/*<Tab.Navigator>*/}
+      {/*  <Tab.Screen*/}
+      {/*    name="Home"*/}
+      {/*    component={HomeScreen}*/}
+      {/*    options={{*/}
+      {/*      tabBarIcon: ({color}) => (*/}
+      {/*        <Icon name="earth" color={color} size={28} />*/}
+      {/*      ),*/}
+      {/*    }}*/}
+      {/*  />*/}
 
-        <Tab.Screen
-          name="WanderList"
-          component={WanderListScreen}
-          options={{
-            tabBarIcon: ({color}) => (
-              <Icon name="list-circle" color={color} size={30} />
-            ),
-          }}
-        />
+      {/*  <Tab.Screen*/}
+      {/*    name="WanderList"*/}
+      {/*    component={WanderListScreen}*/}
+      {/*    options={{*/}
+      {/*      tabBarIcon: ({color}) => (*/}
+      {/*        <Icon name="list-circle" color={color} size={30} />*/}
+      {/*      ),*/}
+      {/*    }}*/}
+      {/*  />*/}
 
-        <Tab.Screen
-          name="Add"
-          component={AddScreen}
-          options={{
-            tabBarIcon: ({color}) => (
-              <Icon name="add-circle" color={color} size={30} />
-            ),
-          }}
-        />
+      {/*  <Tab.Screen*/}
+      {/*    name="Add"*/}
+      {/*    component={AddScreen}*/}
+      {/*    options={{*/}
+      {/*      tabBarIcon: ({color}) => (*/}
+      {/*        <Icon name="add-circle" color={color} size={30} />*/}
+      {/*      ),*/}
+      {/*    }}*/}
+      {/*  />*/}
 
-        <Tab.Screen
-          name="Reward"
-          component={RewardScreen}
-          options={{
-            tabBarIcon: ({color}) => (
-              <Icon name="scan-circle" color={color} size={30} />
-            ),
-          }}
-        />
+      {/*  <Tab.Screen*/}
+      {/*    name="Reward"*/}
+      {/*    component={RewardScreen}*/}
+      {/*    options={{*/}
+      {/*      tabBarIcon: ({color}) => (*/}
+      {/*        <Icon name="scan-circle" color={color} size={30} />*/}
+      {/*      ),*/}
+      {/*    }}*/}
+      {/*  />*/}
 
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            tabBarIcon: ({color}) => (
-              <Icon name="person-circle" color={color} size={30} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+      {/*  <Tab.Screen*/}
+      {/*    name="Profile"*/}
+      {/*    component={ProfileScreen}*/}
+      {/*    options={{*/}
+      {/*      tabBarIcon: ({color}) => (*/}
+      {/*        <Icon name="person-circle" color={color} size={30} />*/}
+      {/*      ),*/}
+      {/*    }}*/}
+      {/*  />*/}
+      {/*</Tab.Navigator>*/}
     </NavigationContainer>
   );
 }
