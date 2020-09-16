e8c71be81249408804f986f7a6092e56915a4367
Steven Yulong Yan <yulong.yan@uqconnect.edu.au>
2020-09-16 11:14:03 +1000

    Add placeholder logo and status bar

--- app/screens/SplashScreen.js
+++ app/screens/SplashScreen.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  StatusBar,
+} from 'react-native';
 
 const SplashScreen = ({navigation}) => {
   return (
     <View>
       <Text>Welcome to WanderList!</Text>
       <View>
+        <StatusBar backgroundColor="#009387" barStyle="light-content" />
         <TouchableOpacity onPress={() => navigation.navigate('Home')}>
           <Text>Get Started</Text>
         </TouchableOpacity>
@@ -15,3 +22,5 @@ const SplashScreen = ({navigation}) => {
 };
 
 export default SplashScreen;
+
+const styles = StyleSheet.create({});
