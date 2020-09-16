50a717405b3278c587f56864e80a9842648c3424
Steven Yulong Yan <yulong.yan@uqconnect.edu.au>
2020-09-16 13:36:34 +1000

    Code cleanup

--- app/screens/SplashScreen.js
+++ app/screens/SplashScreen.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  StatusBar,
-} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 const SplashScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text>Welcome to WanderList!</Text>
       <View>
-        <StatusBar backgroundColor="#009387" barStyle="light-content" />
         <View style={styles.button}>
           <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-            <LinearGradient colors={['#08d4c4', '#01ab9d']}>
-              <Text>Get Started</Text>
-            </LinearGradient>
+            <Text>Get Started</Text>
           </TouchableOpacity>
         </View>
       </View>
--- app/screens/tab-screens/HomeScreen.js
+++ app/screens/tab-screens/HomeScreen.js
@@ -20,28 +20,3 @@ export default function HomeScreen({navigation}) {
     </View>
   );
 }
-
-// export default class HomeScreen extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.logo}>Home Page</Text>
-//       </View>
-//     );
-//   }
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#003f5c',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   logo: {
-//     fontWeight: 'bold',
-//     fontSize: 50,
-//     color: '#fb5b5a',
-//     marginBottom: 40,
-//   },
-// });
