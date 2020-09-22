6aa09fd0a6cdf3b8f2485d0cd2b7b6c43ca43323
johnpaulsmith87 <johnpaulsmith87@users.noreply.github.com>
2020-09-22 21:45:51 +1000

    Update HomeScreen.js
    trying to hooking in api to home screen

--- app/screens/tab-screens/HomeScreen.js
+++ app/screens/tab-screens/HomeScreen.js
@@ -1,21 +1,53 @@
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View, FlatList, Pressable, Image, ImageBackground } from 'react-native';
+import { useSelector } from 'react-redux'
+import CONFIG from '../../config'
+import axios from 'axios'
 
-export default function HomeScreen({navigation}) {
+export default function HomeScreen({ navigation }) {
+
+  const token = useSelector(state => state.userReducer.authToken)
+  const [cities, setCities] = useState(0)
+
+
+  useEffect(() => {
+    async function homeScreenLogic() {
+      //gather cities
+      await axios.get(`${CONFIG.API_URL}city/${contentId}`,
+        { headers: { "Authorization": `Bearer ${token}` } })
+        .then(async (res) => {
+          //expecting a list of cities
+          setCities(res.data)
+        })
+        .catch(async (res) => {
+          console.log('reason: ' + res)
+        })
+    }
+    homeScreenLogic()
+  })
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Wander Screen"
-        onPress={() => navigation.navigate('Wander')}
-      />
-      <Button
-        title="Rewards Screen"
-        onPress={() => navigation.navigate('Rewards')}
-      />
-      <Button
-        title="Profile Screen"
-        onPress={() => navigation.navigate('Profile')}
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <FlatList
+        data={cities}
+        keyExtractor={(city) => city.cityId}
+        renderItem={({ city, index, separators }) => {
+          <ImageBackground
+            source={{
+              uri: `${CONFIG.API_URL}resource/${city.coverImageId}`,
+              headers: { "Authorization": `Bearer ${token}` }
+            }}
+          >
+            <Pressable
+              onPress={() => {
+                navigation.navigate('CityScreen', {
+                  cityId: city.item.cityId
+                })
+              }}>
+              <Text>{city.item.name}</Text>
+            </Pressable>
+          </ImageBackground>
+        }}
       />
     </View>
   );
