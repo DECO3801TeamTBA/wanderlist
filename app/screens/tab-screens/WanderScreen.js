773c9d5d64c40e61816177e3e25e43d85fa0bdfc
Thanh Tran Vo <t.tranvo@uqconnect.edu.au>
2020-09-29 11:26:04 +1000

    wander page search bar and API display testing

--- app/screens/tab-screens/WanderScreen.js
+++ app/screens/tab-screens/WanderScreen.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Text,
+  View,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+  Platform,
+  Button} from 'react-native';
 import { SearchBar } from 'react-native-elements';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
 
 // export default function WanderScreen({navigation}) {
@@ -20,39 +28,198 @@ import { SearchBar } from 'react-native-elements';
 //   );
 // }
 
-export default class WanderScreen extends React.Component {
-  state = {
-    search: '',
+export class WanderScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    //setting default state
+    this.state = { isLoading: true, search: '' };
+    this.arrayholder = [];
+  }
+  componentDidMount() {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: responseJson,
+          },
+          function() {
+            this.arrayholder = responseJson;
+          }
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  search = text => {
+    console.log(text);
+  };
+  clear = () => {
+    this.search.clear();
   };
 
-  updateSearch = (search) => {
-    this.setState({ search });
+  SearchFilterFunction(text) {
+    //passing the inserted text in textinput
+    const newData = this.arrayholder.filter(function(item) {
+      //applying filter for the inserted text in search bar
+      const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+
+    this.setState({
+      //setting the filtered newData on datasource
+      //After setting the data it will automatically re-render the view
+      dataSource: newData,
+      search: text,
+    });
+  }
+
+  ListViewItemSeparator = () => {
+    //Item sparator view
+    return (
+      <View
+        style={{
+          height: 0.3,
+          width: '90%',
+          backgroundColor: '#080808',
+        }}
+      />
+    );
   };
 
   render() {
-    const { search } = this.state;
-
+    if (this.state.isLoading) {
+      //Loading View while data is loading
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
     return (
-      <SearchBar
-        platform='ios'
-        placeholder="Search Destinations and Activities"
-        barTintColor="red"
-        tintColor="green"
-        textColor="blue"
-        textFieldBackgroundColor="pink"
-        hideBackground={false}
-        barStyle="default"
-        searchBarStyle="default"
-        onChangeText={this.updateSearch}
-        value={search}
-      />
+      //ListView to show with textinput used as search bar
+      
+      <View style={styles.viewStyle}>
+              <View style={styles.searchStyle}>
+
+          
+        </View>
+        <View style={{height: 50}}>
+          <SearchBar
+            showLoading={false}
+            platform={Platform.OS}
+            clearIcon={true}            round
+            searchIcon={{ size: 20 }}
+            onChangeText={text => this.SearchFilterFunction(text)}
+            onClear={text => this.SearchFilterFunction('')}
+            placeholder="Search Your Lists"
+            value={this.state.search}
+          />
+        </View>
+        <View style={{height: 50}} >
+
+          <Text style={styles.bigBlack}>
+            Your Lists
+          </Text>
+          
+
+        </View>
+
+          <FlatList
+            data={this.state.dataSource}
+            ItemSeparatorComponent={this.ListViewItemSeparator}
+            //Item Separator View
+            renderItem={({ item }) => (
+              // Single Comes here which will be repeatative for the FlatListItems
+              <View style={styles.card}>
+
+                <Text style={styles.textStyle}>{item.title}</Text>
+              </View>
+
+            )}
+            enableEmptySections={true}
+            style={{ marginTop: 10 }}
+            keyExtractor={(item, index) => index.toString()}
+          />
+
+      </View>
     );
   }
 }
 
-
-
-
 const styles = StyleSheet.create({
-
+  viewStyle: {
+    justifyContent: 'center',
+    flex: 1,
+    
+    backgroundColor: 'white',
+    // marginTop: Platform.OS == 'ios' ? 30 : 0,
+  },
+  searchStyle: {
+    justifyContent: 'center',
+    flex: 2,
+    backgroundColor: 'white',
+    // marginTop: Platform.OS == 'ios' ? 30 : 0,
+  },
+  textStyle: {
+    padding: 10,
+  },
+  card: {
+    height: 200,
+    marginVertical: 30,
+    marginHorizontal: 20,
+    flexDirection: 'row',
+    shadowColor: '#999',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+    backgroundColor: '#fff',
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    width: '90%',
+    alignSelf: 'center',
+    marginTop: 25,
+    marginBottom: 10,
+  },
+  bigBlack: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginHorizontal: 20,
+  },
+  cardInfo: {
+    flex: 2,
+    padding: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderBottomRightRadius: 8,
+    borderTopRightRadius: 8,
+    backgroundColor: '#fff',
+  },
 });
+
+
+const mapStateToProps = (state) => {
+
+  return {
+    user: state.userReducer.user
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    attachUser: (user) => dispatch(setUser(user)),
+    attachToken: (authToken) => dispatch(setToken(authToken)),
+    attachExpiry: (expiry) => dispatch(setExpiry(expiry))
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(WanderScreen);
